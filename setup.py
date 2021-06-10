89828f998c9db87c830bdde39af1eab987074c75
LXZ <xuzhu.liu@avazu.net>
2021-06-10 10:24:47 +0800

    更新结构

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='justauth',
+    version='0.0.1',
+    keywords='auth',
+    description='Aggregate authorization extension libraries',
+    license='MIT License',
+    url='https://github.com/justauth/justauth-python',
+    author='PFinal南丞',
+    author_email='lampxiezi@163.com',
+    packages=find_packages(),
+    include_package_data=True,
+    platforms='any',
+    install_requires=[],
+)
